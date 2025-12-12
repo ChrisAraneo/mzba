dd89730c6befa25dd49b4d388d8a600c6aaa47fe
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-12 20:40:20 +0100

    feat(server): initialize web socket support, create example endpoint

--- mzba-game-server/src/main/scala/Main.scala
+++ mzba-game-server/src/main/scala/Main.scala
@@ -1,3 +1,39 @@
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.http.scaladsl.Http
+import org.apache.pekko.http.scaladsl.model.ws.{Message, TextMessage}
+import org.apache.pekko.http.scaladsl.server.Directives._
+import org.apache.pekko.stream.scaladsl.Flow
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
 object Main extends App {
-  println("Welcome to mzba-game-server!")
+  implicit val system: ActorSystem = ActorSystem("mzba-game-server")
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val websocketFlow: Flow[Message, Message, Any] = Flow[Message].map {
+    case TextMessage.Strict(text) =>
+      val response = if (text == "hi") "hello" else s"Echo: $text"
+      TextMessage(response)
+    case _ => TextMessage("Unsupported message type")
+  }
+
+  val route =
+    path("ws") {
+      get {
+        handleWebSocketMessages(websocketFlow)
+      }
+    }
+
+  val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+  println("WebSocket server started at ws://localhost:8080/ws")
+  println("Send 'hi' to receive 'hello' response")
+  println("Press ENTER to stop...")
+
+  StdIn.readLine()
+  
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
 }
